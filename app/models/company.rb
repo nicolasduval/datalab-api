8c5f46bd5f0520883d24ca301bd35d39817db236
Nicolas Duval <nduval.dev@gmail.com>
2016-02-16 14:47:22 +0100

    clean code ( issue #12 )

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,6 @@
 class Company < ActiveRecord::Base
 
-  has_many  :projects, dependent: :destroy
+  has_many :projects, dependent: :destroy
   has_and_belongs_to_many :users
   validates :name, presence: true, uniqueness: true
 
